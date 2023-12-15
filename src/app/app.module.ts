b20154a6c5c29fb8064832e64228710ae508a50c
nadir inab <nadir.inab.dev@gmail.com>
2023-12-15 11:12:29 +0100

    code is well added , and style is cool

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { PodiumComponent } from './components/podium/podium.component';
+import { CompetitionComponent } from './components/competition/competition.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavComponent,
+    FooterComponent,
+    PodiumComponent,
+    CompetitionComponent
   ],
   imports: [
     BrowserModule,
